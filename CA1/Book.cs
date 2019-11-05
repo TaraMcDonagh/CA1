713f064cbf6cea90bd2bd5d30cea900773688902
s00185883 <s00185883@LAB-B1042-19>
2019-11-05 15:37:58 +0000

    Doing Bits

---
+++ CA1/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Book
+    {
+         /*Provide public accessors for these properties with auto-implemented Getters 
+        and Setters for each*/
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public int Pages { get; set; }
+        public string Genre { get; set; }
+
+        public Book(string author, string title, DateTime published,int pages,string genre)
+        {
+            Title = title;
+            Author = author;
+            Published = published;
+            Pages = pages;
+            Genre = genre;
+        }
+        public override string ToString()
+        {
+            return string.Format($"");
+        }
+    }
+
+    //This is what i found while googling enum but i dont quite understand it so we're just going to use a string.
+    enum Genre
+    {
+        Fiction,
+        Biography,
+        Computing,
+        Literature
+    }
+}
--- CA1/Program.cs
+++ CA1/Program.cs
@@ -10,6 +10,12 @@ namespace CA1
     {
         static void Main(string[] args)
         {
+            //create 5 Player objects
+            Book Book1 = new Book("James Joyce", "Ulysses", new DateTime(1922, 02, 02),730, "Literature");
+            Book Book2 = new Book("B Kerighan and D Ritchie ", "The C Programming Language", new DateTime(1978, 02, 28), 279, "Computing");
+            Book Book3 = new Book("Walter Isaacson", "Steve Jobs", new DateTime(2011, 10, 24), 627, "Biography");
+            Book Book4 = new Book("JRR Tolkin", "The Hobbit", new DateTime(1937, 09, 21), 210, "Fiction");
+            Book Book5 = new Book("James Joyce", "Portrait of the Artist", new DateTime(1922, 02, 02), 730, "Literature");
         }
     }
 }
