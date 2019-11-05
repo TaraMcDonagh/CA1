7ae477d22c91f77d2bd56467193356dd5a608303
s00185883 <s00185883@LAB-B1042-19>
2019-11-05 16:32:24 +0000

    Test

--- CA1/Book.cs
+++ CA1/Book.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 
 namespace CA1
 {
-    class Book
+    //2.Create a class called Book.
+    class Book : IComparable<Book>
     {
-         /*Provide public accessors for these properties with auto-implemented Getters 
-        and Setters for each*/
+/*4.Add the following Properties to the Book class – Title[string], 
+ * Author[string], Published[DateTime], Pages[int] and Genre[Genre].*/
         public string Title { get; set; }
         public string Author { get; set; }
         public DateTime Published { get; set; }
         public int Pages { get; set; }
         public string Genre { get; set; }
-
+        public int Age { get; set; }
+        //5.Create 3 constructors.  Link these constructors using this.
         public Book(string author, string title, DateTime published,int pages,string genre)
         {
             Title = title;
@@ -24,13 +26,30 @@ namespace CA1
             Pages = pages;
             Genre = genre;
         }
+        public Book (int age)
+        {
+            
+        }
+        //6.Override the ToString() method to display all properties.
         public override string ToString()
         {
-            return string.Format($"");
+            return string.Format($"{Author,-30}{Title,-30}{Pages,-15}{Genre,-15}{Published,-30}");
+        }
+        /*10.Add IComparable to enable you to sort the books by Author then 
+         * Title.  Change the books to ensure two books are by the same author.*/
+        public int CompareTo(Book other)
+        {
+            if (this.Author == other.Author)
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+            return other.Author.CompareTo(this.Author);
         }
     }
 
-    //This is what i found while googling enum but i dont quite understand it so we're just going to use a string.
+    /*3.Add an enum called Genre which has the following options – 
+     * Fiction/Biography/Computing/Literature. This is what i found while 
+     * googling enum but i dont quite understand it so we're just going to use a string.*/
     enum Genre
     {
         Fiction,
--- CA1/Program.cs
+++ CA1/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*  Student Number: S00185883   *
+ *     Name: Tara McDonagh      *
+ *            CA1               *
+ *      Date: 05/11/2019        */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +16,60 @@ namespace CA1
     {
         static void Main(string[] args)
         {
-            //create 5 Player objects
+      //7.Create 5 Book objects, you can use your own data or that in the screenshot.
             Book Book1 = new Book("James Joyce", "Ulysses", new DateTime(1922, 02, 02),730, "Literature");
             Book Book2 = new Book("B Kerighan and D Ritchie ", "The C Programming Language", new DateTime(1978, 02, 28), 279, "Computing");
             Book Book3 = new Book("Walter Isaacson", "Steve Jobs", new DateTime(2011, 10, 24), 627, "Biography");
             Book Book4 = new Book("JRR Tolkin", "The Hobbit", new DateTime(1937, 09, 21), 210, "Fiction");
-            Book Book5 = new Book("James Joyce", "Portrait of the Artist", new DateTime(1922, 02, 02), 730, "Literature");
+            Book Book5 = new Book("James Joyce", "Portrait of the Artist", new DateTime(1916, 12, 29), 299, "Literature");
+
+            //8.Add these Book objects to a List called ReadingList
+            List<Book> ReadingList = new List<Book>();
+            ReadingList.Add(Book1);
+            ReadingList.Add(Book2);
+            ReadingList.Add(Book3);
+            ReadingList.Add(Book4);
+            ReadingList.Add(Book5);
+            //calling the Display method
+            Display(ReadingList);
+
+            //Sorting through the list and putting them in order by author
+            ReadingList.Sort();
+            Console.WriteLine("Sorted Books");
+            foreach (var book in ReadingList)
+            {
+                Console.WriteLine(book);
+            }
+            //Displaying ages of books
+            DisplayWithAge(ReadingList);
+
+        }
+        //9.Write a method in Program.cs which displays the books in the reading list.
+        private static void Display(List<Book> books)
+        {
+            Console.WriteLine("{0,-30}{1,-30}{2,-15}{3,-15}{4,-30}", "Author", "Title", "Pages", "Genre", "Publication Date");
+            foreach (Book book in books)
+            {
+                Console.WriteLine("{0,-10}", book);
+            }
+
+            Console.WriteLine();
+
+        }
+        /*11.In Program.cs write a method called DisplayWithAge which will look at each 
+         * book and calculate the age in years of the book and display it .*/
+         private static void DisplayWithAge(List<Book> books)
+        {
+            Console.WriteLine("Ages");
+            foreach (Book book in books)
+            {
+                int years = DateTime.Now.Year - book.Published.Year;
+                Console.WriteLine(years);
+            }
+
+            Console.WriteLine();
+            
+
         }
     }
 }
